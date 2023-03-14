791f34acf188b3dc94a6f2d3a34584450d569c42
Averuki <averuki@gmail.com>
2023-03-14 17:20:29 +0100

    Update AveFeignDeathFix.lua

--- AveFeignDeathFix.lua
+++ AveFeignDeathFix.lua
@@ -1,35 +1,18 @@
 local UnitIsPlayer = UnitIsPlayer
 local UnitIsUnit   = UnitIsUnit
 local C_NamePlate  = C_NamePlate
-local FDF = CreateFrame("Frame")
-FDF:RegisterEvent("NAME_PLATE_UNIT_ADDED")
-FDF:RegisterEvent("NAME_PLATE_UNIT_REMOVED")
-FDF:SetScript("OnEvent", function(self, event, ...)
-	if event == "NAME_PLATE_UNIT_ADDED" then
-		local unit = ...
-		self:OnNamePlateAdded(unit)
-	elseif event == "NAME_PLATE_UNIT_REMOVED" then
-		local unit = ...
-		self:OnNamePlateRemoved(unit)
-    end
+
+EventRegistry:RegisterFrameEventAndCallback("NAME_PLATE_UNIT_ADDED", function(owner, ...)
+	local unit = ...
+	local nameplate = C_NamePlate.GetNamePlateForUnit(unit)
+	if UnitIsPlayer(unit) and (select(3, UnitClass(unit)) == 3) and not UnitIsUnit(unit, "player") then
+		nameplate.aveFeignDeath = true
+		nameplate.UnitFrame.healthBar:SetScript("OnHide", function(self) if nameplate.aveFeignDeath then self:Show() end end)
+	end
 end)
 
-local function feignDeathFix(unit, nameplate)
-	if select(3, UnitClass(unit)) == 3 then
-		nameplate.AveFeignDeath = true
-	end
-end
-
-function FDF:OnNamePlateAdded(unit)
-	local unit = unit
+EventRegistry:RegisterFrameEventAndCallback("NAME_PLATE_UNIT_REMOVED", function(owner, ...)
+	local unit = ...
 	local nameplate = C_NamePlate.GetNamePlateForUnit(unit)
-	nameplate.UnitFrame.healthBar:SetScript("OnHide", function(self) if nameplate.AveFeignDeath then self:Show() end end)
-	if UnitIsPlayer(unit) and not UnitIsUnit("player", unit) then
-		feignDeathFix(unit, nameplate)
-    end
-end
-
-function FDF:OnNamePlateRemoved(unit)
-	local nameplate = C_NamePlate.GetNamePlateForUnit(unit)
-	nameplate.AveFeignDeath = nil
-end
+	nameplate.aveFeignDeath = nil
+end)
